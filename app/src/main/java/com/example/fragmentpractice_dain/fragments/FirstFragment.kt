fad397eff945f97a00ae792780054507542a8b3b
DI <cdi122@nate.com>
2022-08-05 22:26:39 +0900

    토스트 띄우기 예시  - context 변수 활용

--- app/src/main/java/com/example/fragmentpractice_dain/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/fragmentpractice_dain/fragments/FirstFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.fragmentpractice_dain.R
 import kotlinx.android.synthetic.main.fragment_first.*
@@ -22,6 +23,9 @@ class FirstFragment : Fragment () {
         super.onActivityCreated(savedInstanceState)
         changeTextBtn.setOnClickListener {
             contentTxt.text = "안녕하세요"
+        }
+        toastBtn.setOnClickListener {
+            Toast.makeText(context, "프래그먼트의 토스트", Toast.LENGTH_SHORT).show()
         }
     }
 }
