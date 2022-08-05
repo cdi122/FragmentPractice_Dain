136f13c0b2d091888f812fab0042bd8810bb1015
DI <cdi122@nate.com>
2022-08-05 22:20:08 +0900

    첫 프래그먼트 동작처리 + 메인화면 xml 배치

--- app/src/main/java/com/example/fragmentpractice_dain/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/fragmentpractice_dain/fragments/FirstFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.fragmentpractice_dain.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment () {
 
@@ -17,4 +18,10 @@ class FirstFragment : Fragment () {
         return inflater.inflate(R.layout.fragment_first, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        changeTextBtn.setOnClickListener {
+            contentTxt.text = "안녕하세요"
+        }
+    }
 }
